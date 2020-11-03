6956b7a940cbe4b6a99a58ee1c1fe2eee2d11ba7
Mariana Costa <mariana.costa214@gmail.com>
2020-11-03 12:15:00 +0000

    Add state machine and register laser readings

--- reactive_robot/src/wall_following/main.py
+++ reactive_robot/src/wall_following/main.py
@@ -17,26 +17,78 @@ from geometry_msgs.msg import Twist
 SPEED = 0.2
 ANGULAR_SPEED = 0.4
 
+state = {
+    'FIND': 0,
+    'GO': 1,
+    'FOLLOW': 2
+}
+current_state = state['FIND']
+
+laser_readings = {
+    'ranges': [],
+    'range_max': 0,
+    'range_min': 0,
+    'angle_increment': 0,
+    'angle_min': 0,
+    'angle_max': 0,
+    'scan_time': 0,
+    'time_increment': 0,
+    'intensities': [],
+}
+
+def find():
+    msg_to_send = Twist()
+    msg_to_send.linear.x = SPEED
+    msg_to_send.angular.z = ANGULAR_SPEED
+    return msg_to_send
+
+def go():
+    return None
+
+def follow():
+    return None
+
 def callback(data):
-    pass
+    global laser_readings
+
+    laser_readings['ranges'] = data.ranges
+    laser_readings['range_max'] = data.range_max
+    laser_readings['range_min'] = data.range_min
+    laser_readings['angle_increment'] = data.angle_increment
+    laser_readings['scan_time'] = data.scan_time
+    laser_readings['time_increment'] = data.time_increment
+    laser_readings['angle_min'] = data.angle_min
+    laser_readings['angle_max'] = data.angle_max
+    laser_readings['intensities'] = data.intensities
 
 def wall_following(robot_frame_id, laser_frame_id):
-    global SPEED, ANGULAR_SPEED
-    
+    global SPEED, ANGULAR_SPEED, current_state, laser_readings
+
     rospy.init_node('wall_following', anonymous=True)
     rospy.Subscriber(robot_frame_id + '/' + laser_frame_id, LaserScan, callback)
     publisher = rospy.Publisher(robot_frame_id + '/cmd_vel', Twist, queue_size=1)
 
     rate = rospy.Rate(20)
     while not rospy.is_shutdown():
-        msg_to_send = Twist()
-        msg_to_send.linear.x = SPEED
-        msg_to_send.angular.z = ANGULAR_SPEED
-        publisher.publish(msg_to_send)
+        msg_to_send = None
+
+        if current_state == state['FIND']:
+            msg_to_send = find()
+        elif current_state == state['GO']:
+            msg_to_send = go()
+        elif current_state == state['FOLLOW']:
+            msg_to_send = follow()
+        else:
+            pass
+
+        if (msg_to_send is not None):
+            publisher.publish(msg_to_send)
         rate.sleep()
 
 if __name__ == '__main__':
+    robot = sys.argv[1]
+    laser = sys.argv[2]
     try:
-        wall_following(sys.argv[1], sys.argv[2])
+        wall_following(robot, laser)
     except rospy.ROSInterruptException:
         pass
